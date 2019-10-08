e0f44bdd020e5d6eff2ffe1d00ef521931be9942
Ramon-Deniz <ramondeniz4@gmail.com>
2019-10-08 07:43:25 -0700

    Documentation added

---
+++ cse360Assign2/AddingMachine.java
@@ -0,0 +1,53 @@
+package cse360assign2;
+
+public class AddingMachine {
+
+	// keeps track of the total sum
+	private int total;
+
+	/**
+	 * Used to initialize an AddingMachine object
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+
+	/**
+	 *
+	 * @return the total sum
+	 */
+	public int getTotal () {
+		return 0;
+	}
+
+	/**
+	 * adds a value to the current total
+	 * @param value, the integer being added
+	 */
+	public void add (int value) {
+
+	}
+
+	/**
+	 * subtracts a value from the current total
+	 * @param value, the integer subtracting from total
+	 */
+	public void subtract (int value) {
+
+	}
+
+	/**
+	 *
+	 * @return a String representation of the arithmetic operations
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * Resets the total and resets the String representation of the arithmetic operations
+	 */
+	public void clear() {
+	
+	}
+}
